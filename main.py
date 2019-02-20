a9bb9a10d2e55227dabd8fa710ea0c61f822dce6
Bjornir90 <celestincollin@gmail.com>
2019-02-20 16:26:34 +0100

    Enhanced precision of result

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Y = np.load('images_et4/data/trn_lbl.npy')
 classe=[]
 moyenne=[]
 #barycentres
-for i in range(9):
+for i in range(10):
     classe.append(X[Y == i])
     moyenne.append(np.mean(classe[i], axis=0))
 
@@ -16,11 +16,12 @@ dev = np.load('images_et4/data/dev_img.npy')
 devLabel = np.load('images_et4/data/dev_lbl.npy')
 
 
+
 result = []
 for j in range(5000):
     minimumDistance = 10000000
     minimumIndex = 0
-    for i in range(9):
+    for i in range(10):
         distance = np.linalg.norm(dev[j] - moyenne[i])
         if distance < minimumDistance:
             minimumIndex = i
