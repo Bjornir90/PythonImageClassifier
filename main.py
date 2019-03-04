5df40e908ab6ad69b9b1a64ba106f0b3acb3df81
Bjornir90 <celestincollin@gmail.com>
2019-03-04 15:45:05 +0100

    Some finishings

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 X = np.load('images_et4/data/trn_img.npy')
 Y = np.load('images_et4/data/trn_lbl.npy')
 
-
 classe=[]
 moyenne=[]
+
 #barycentres
 for i in range(10):
     classe.append(X[Y == i])
@@ -16,9 +15,8 @@ for i in range(10):
 dev = np.load('images_et4/data/dev_img.npy')
 devLabel = np.load('images_et4/data/dev_lbl.npy')
 
-
-
 result = []
+
 for j in range(5000):
     minimumDistance = 10000000
     minimumIndex = 0
@@ -34,8 +32,8 @@ print("Taux d'images non reconnues : ")
 # Most beautiful thing ever
 print(finalResult[finalResult].size/finalResult.size)
 
-values = [10,20,50,100,300,400,500,700,784]
-for k in (values):
+values = [10, 20, 50, 100, 250, 300, 350, 400, 500]
+for k in values:
     pca = PCA(n_components=k)
     pca.fit(X)
     reducedX = pca.transform(X)
