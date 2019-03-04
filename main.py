3db611523a280ed6f0917165c664ce6d4b03605c
Bjornir90 <celestincollin@gmail.com>
2019-03-04 14:53:32 +0100

    Question 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 X = np.load('images_et4/data/trn_img.npy')
 Y = np.load('images_et4/data/trn_lbl.npy')
@@ -32,5 +33,37 @@ finalResult = result != devLabel
 print("Taux d'images non reconnues : ")
 # Most beautiful thing ever
 print(finalResult[finalResult].size/finalResult.size)
+
+values = [10,20,50,100,300,400,500,700,784]
+for k in (values):
+    pca = PCA(n_components=k)
+    pca.fit(X)
+    reducedX = pca.transform(X)
+
+    classe=[]
+    moyenne=[]
+    #barycentres
+    for i in range(10):
+        classe.append(reducedX[Y == i])
+        moyenne.append(np.mean(classe[i], axis=0))
+
+    result = []
+    reducedDev = pca.transform(dev)
+
+    for j in range(5000):
+        minimumDistance = 10000000
+        minimumIndex = 0
+        for i in range(10):
+            distance = np.linalg.norm(reducedDev[j] - moyenne[i])
+            if distance < minimumDistance:
+                minimumIndex = i
+                minimumDistance = distance
+        result.append(minimumIndex)
+
+    finalResult = result != devLabel
+    print("Taux d'images non reconnues : ")
+    # Most beautiful thing ever
+    print(finalResult[finalResult].size/finalResult.size, "with nb components = ", k)
+
 
 
